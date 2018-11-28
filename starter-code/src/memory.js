2d17983009d95eebebaedf174dc40318c73ac04b
guilhermeaeme <guilhermeaeme@gmail.com>
2018-11-28 20:53:49 -0200

    jasmine

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,29 @@
 var MemoryGame = function (cards) {
-  this.cards = cards;
+  this.cards = cards,
+  this.pickedCards = [],
+  this.pairsClicked = 0,
+  this.pairsGuessed = 0
 };
 
 MemoryGame.prototype.shuffleCards = function () {
+	for (var i = this.cards.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = this.cards[i];
+        this.cards[i] = this.cards[j];
+        this.cards[j] = temp;
+    }
 };
 
 MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-}
+	var result = (firstCard === secondCard);
+
+	this.pairsClicked++;
+
+	if(result) this.pairsGuessed++;
+
+	return result;
+};
 
 MemoryGame.prototype.isFinished = function () {
+	return this.pairsGuessed == (this.cards.length / 2);
 };
